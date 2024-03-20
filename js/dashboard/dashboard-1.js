05df2ea8617c805bc1e1733387b5bdb17f6483e2
JuandavidP18 <jdsierra482@misena.edu.co>
2024-03-20 06:37:06 -0500

    once

--- js/dashboard/dashboard-1.js
+++ js/dashboard/dashboard-1.js
@@ -272,12 +272,12 @@
 		var options = {
 			  series: [
 				{
-					name: 'Running',
+					name: 'Realiza',
 					data: [50, 18, 70, 40, 90, 70, 20],
 					//radius: 12,	
 				}, 
 				{
-				  name: 'Cycling',
+				  name: 'CIclo',
 				  data: [80, 40, 55, 20, 45, 30, 80]
 				}, 
 				
@@ -342,13 +342,13 @@
 			},
 			xaxis: {
 				 position: 'bottom',
-			  categories: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+			  categories: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
 			  labels: {
 			   style: {
 				  colors: '#787878',
 				  fontSize: '13px',
 				  fontFamily: 'poppins',
-				  fontWeight: 100,
+				  fontWeight: 80,
 				  cssClass: 'apexcharts-xaxis-label',
 				},
 			  },
@@ -385,7 +385,7 @@
 			tooltip: {
 			  y: {
 				formatter: function (val) {
-				  return "$ " + val + " thousands"
+				  return "$ " + val + " ventas"
 				}
 			  }
 			},
@@ -851,130 +851,6 @@
 	 
 	}
 	
-	var redial = function(){
-		  var options = {
-          series: [70],
-          chart: {
-          type: 'radialBar',
-          offsetY: 0,
-		  height:350,
-          sparkline: {
-            enabled: true
-          }
-        },
-        plotOptions: {
-          radialBar: {
-            startAngle: -130,
-            endAngle: 130,
-            track: {
-              background: "#F1EAFF",
-              strokeWidth: '100%',
-              margin: 5,
-            },
-			
-			hollow: {
-              margin: 30,
-              size: '45%',
-              background: '#F1EAFF',
-              image: undefined,
-              imageOffsetX: 0,
-              imageOffsetY: 0,
-              position: 'front',
-            },
-			
-            dataLabels: {
-              name: {
-                show: false
-              },
-              value: {
-                offsetY: 5,
-                fontSize: '22px',
-				color:'#886CC0',
-				fontWeight:700,
-              }
-            }
-          }
-        },
-		responsive: [{
-          breakpoint: 1600,
-          options: {
-           chart: {
-			  height:250
-			},
-          }
-        }
-		
-		],
-        grid: {
-          padding: {
-            top: -10
-          }
-        },
-		/* stroke: {
-          dashArray: 4,
-		  colors:'#6EC51E'
-        }, */
-        fill: {
-          type: 'gradient',
-		  colors:'#FF63E6',
-          gradient: {
-              shade: 'white',
-              shadeIntensity: 0.15,
-              inverseColors: false,
-              opacityFrom: 1,
-              opacityTo: 1,
-              stops: [0, 50, 65, 91]
-          },
-        },
-        labels: ['Average Results'],
-        };
-
-        var chart = new ApexCharts(document.querySelector("#redial"), options);
-        chart.render();
-	
-	
-	}
-	var emailchart = function(){
-		 var options = {
-          series: [27, 11, 22,15,25],
-          chart: {
-          type: 'donut',
-		  height:300
-        },
-		dataLabels:{
-			enabled: false
-		},
-		stroke: {
-          width: 0,
-        },
-		colors:['var(--primary)', '#26E023', '#61CFF1','#FFDA7C','#FF86B1'],
-		legend: {
-              position: 'bottom',
-			  show:false
-            },
-        responsive: [{
-          breakpoint: 1800,
-          options: {
-           chart: {
-			  height:200
-			},
-          }
-        },
-		{
-          breakpoint: 1800,
-          options: {
-           chart: {
-			  height:200
-			},
-          }
-        }
-		]
-        };
-
-        var chart = new ApexCharts(document.querySelector("#emailchart"), options);
-        chart.render();
-    
-	}
 	
 	
 	
